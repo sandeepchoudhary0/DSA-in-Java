dfcae168f833d19b92bb162c1d656735c3efe453
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-03-06 21:42:32 +0530

    Update Pro1.java

--- Pro1.java
+++ Pro1.java
@@ -2,7 +2,17 @@ import java.util.*;
 
 public class Pro1 {
     public static void main(String[] args) {
+        int sum=0;
         Scanner input= new Scanner(System.in);
-        
+        System.out.println("Enter the number of Days: ");
+        int Days= input.nextInt();
+        for (int i = 0; i < Days; i++) {
+            Scanner consol = new Scanner(System.in);
+            System.out.println("The Temperature of day "+(i+1)+ " is: ");
+            int Temp= consol.nextInt();
+            sum=sum+Temp;
+            
+        }
+        System.out.println("The total temperature is "+sum);
     }
 }
