8ebedf1dffc865f624809c1e7e5f872868ae15d5
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-03-08 21:25:51 +0530

    Compleate Pro1.java

--- Pro1.java
+++ Pro1.java
@@ -6,6 +6,7 @@ public class Pro1 {
         Scanner input= new Scanner(System.in);
         System.out.println("Enter the number of Days: ");
         int Days= input.nextInt();
+        int[] Temp= new int[Days];
         for (int i = 0; i < Days; i++) {
             Scanner consol = new Scanner(System.in);
             System.out.println("The Temperature of day "+(i+1)+ " is: ");
@@ -13,14 +14,15 @@ public class Pro1 {
             sum=sum+Temp[i];
             
         }
-        {int average= sum/Days;}
+        int average= sum/Days;
+        System.out.println("The average temperature of all days is :"+average);
         for (int i = 0;  i < Temp.length; i++) {
             if (Temp[i]>average) {
-                System.out.println("The temperature more then average temperature are on "+Temp[i]+". Days "+i+".");
+                System.out.println("The temperature more then average temperature is "+Temp[i]+", on Day "+(i+1)+".");
                 
             }
             
         }
-        System.out.println("The total temperature is "+sum);
+        //System.out.println("The total temperature is "+sum);
     }
 }
