f6868f10546b66fc01aaca32e5eb435aa69f5bf0
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-03-10 23:30:43 +0530

    Create DiagonalSum2D.java
    Calculating the sum of diagonal elements in a Two Dimensional Array.

---
+++ DiagonalSum2D.java
@@ -0,0 +1,17 @@
+public class Exercise {
+    public static int sumDiagonalElements(int[][] array){
+
+        int sum=0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                while ( i==j ) 
+                {
+                    sum=sum+array[i][j];
+                    break;
+                }
+            }
+        }
+        return sum;
+    }
+}
