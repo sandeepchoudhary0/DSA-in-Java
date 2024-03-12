664f210df46c4d94fe7fb294ea19310a029766bc
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-03-12 22:52:45 +0530

    Update Best Score.java
    Algorithm to print the First and Second Highest Value of a given Array in a new Array.

--- Best Score.java
+++ Best Score.java
@@ -1,6 +1,18 @@
+import java.util.*;
 public class best {
     public static int[] findTopTwoScores(int[] array){
-  
+      int first= Integer.MIN_VALUE;
+      int second= Integer.MIN_VALUE;
+      for (int score : array) {
+        if (score>first) {
+          second=first;
+          first=score;
+        } else if (score>second && score<first){
+          first=second;
+          
+        }
+      }
+      return new int[]{first+second};
     }
   
   }
