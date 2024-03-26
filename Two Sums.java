29ec02d0eb8b19f8e10228ab3e29d8d556e68a2a
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-03-27 00:13:52 +0530

    Update Two Sums.java
    Sun of two integer elements resulting in Target.

--- Two Sums.java
+++ Two Sums.java
@@ -1,12 +1,12 @@
-public class twos {
+public class Exercise {
     public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i < nums.length-1; i++) {
         for (int j = i+1; j < nums.length; j++) {
           if (nums[i]+nums[j]==target) {
-            return  new int[]{nums[i],nums[j]};
+            return new int[] {i,j};
           }
-            
+
         }
-    }
+    }throw new IllegalArgumentException("No two sum solution found");
     }
 }
