55c44d4e40feacd0382ca1611524c507092b1b17
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-03-26 23:54:55 +0530

    Update Two Sums.java
    Sum of two element which will result in our target value.

--- Two Sums.java
+++ Two Sums.java
@@ -1,12 +1,12 @@
-public class twoSums {
+public class twos {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[] { i, j };
-                }
-            }
+    for (int i = 0; i < nums.length-1; i++) {
+        for (int j = i+1; j < nums.length; j++) {
+          if (nums[i]+nums[j]==target) {
+            return  new int[]{i,j};
+          }
+            
         }
-        throw new IllegalArgumentException("No two sum solution found");
+    }
     }
 }
