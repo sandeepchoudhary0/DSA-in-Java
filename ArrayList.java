f80e4d6f7966e055bc0e634353c6f93f62d9606c
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-04-18 23:32:32 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -1,19 +1,18 @@
-package ArrayList;
 import java.util.*;
-
-public class arrayL {
+public class main {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers= new ArrayList<Integer>();
-        numbers.add(12);
-        numbers.add(13);
-        numbers.add(14);
-        System.out.println(numbers);
-        //insertion
-        numbers.add(2,5);
-        System.out.println(numbers);
-        //accessing
-        System.out.println(numbers.get(0));
+        ArrayList ar = new ArrayList(Arrays.asList(13,12,12));
+        System.out.println(ar);
+        //Insertion
+        ar.add(2,3);
+        System.out.println(ar);
+        //Accessing
+        System.out.println(ar.get(0));
+        System.out.println(ar.get(1));
+        //Traversing
+        for (int i = 0; i <ar.size(); i++) {
+            System.out.println( ar.get(i));
+            
+        }
     }
 }
-
-
