c736dadaebb39396e5395994d49151bee193f0a7
Sandeep Choudhary <140533768+sandeepchoudhary0@users.noreply.github.com>
2024-04-24 23:50:14 +0530

    Update ArrayList.java
    Performing Various operations such as Insertion, Accessing, Traversing, and Deletion on ArrayList.

--- ArrayList.java
+++ ArrayList.java
@@ -1,36 +1,30 @@
-package ArrayList;
 import java.util.*;
-
-public class arrayL {
+public class main {
     public static void main(String[] args) {
-        ArrayList<Integer> numbers= new ArrayList<Integer>();
-        numbers.add(12);
-        numbers.add(13);
-        numbers.add(14);
-        System.out.println(numbers);
-        //insertion
-        numbers.add(2,5);
-        System.out.println(numbers);
-        //accessing
-        System.out.println(numbers.get(0));
-        //traversing
-        for (int i = 0; i < numbers.size(); i++) {
-            System.out.println(numbers.get(i));   
-        }
-        //searching
-        ArrayList<String> stringList = new ArrayList<String>(Arrays.asList("A","B"));   
-        for (String letters : stringList) {
-            if(letters.equals("A")){
-                System.out.println("The value exists!");
-              
+        ArrayList ar = new ArrayList(Arrays.asList(13,12,12));
+        System.out.println(ar);
+        //Insertion
+        ar.add(2,3);
+        System.out.println(ar);
+        //Accessing
+        System.out.println(ar.get(0));
+        System.out.println(ar.get(1));
+        //Traversing
+        for (int i = 0; i <ar.size(); i++) {
+            System.out.println( ar.get(i));}
+        ArrayList<String> stringlist = new ArrayList<String>(Arrays.asList("A","B","C","D"));
+        for(String letters:stringlist){
+            if (letters.equals("A")) {
+                System.out.println("It is present in the LIST");
+                break;
+                
             }
-            //indexOf
-            int index = stringList.indexOf("B");
-            System.out.println("The element is found at index of " + index);
-            break;
-        }      
-        
+            System.out.println(letters.indexOf("A"));
+        }
+        //Deletion
+        stringlist.remove("A");
+        System.out.println(stringlist);
+        stringlist.remove(1);
+        System.out.println(stringlist);
+        }
     }
-}
-
-
